0365adb17800fd9419f401501aa347c3eb52c9a8
Muhammad Fikar Firdiansyah <155887932+Feekarr-13@users.noreply.github.com>
2025-08-09 11:29:22 +0700

    Initial commit (reset history)

---
+++ src/components/banner.jsx
@@ -0,0 +1,27 @@
+import { FaLocationDot } from 'react-icons/fa6'
+import style from "../styles"
+import { uinimg } from "../assets"
+
+const Banner = () => {
+
+  return (
+    <div className={`${style.paddingX} h-[400px] !bg-no-repeat !bg-cover bg-fixed bg-center`} style={{ backgroundImage: `url(${uinimg})` }} >
+      <div className="max-w-[1300px] h-full text-white m-auto flex flex-col justify-center items-start">
+        <h2 className={`${style.heading2} mb-4`}>Gratis Ongkir</h2>
+        <p className={`${style.paragraph} mb-10 max-w-[557px]`}>
+          Nikmati Zhavana Banana Chips dalam berbagai keadaan. Pengiriman <strong>
+            gratis
+          </strong> untuk daerah <strong>
+            Desa Gondanglegi
+          </strong> dan sekitarnya. Dapatkan Zahavana Banana Chips di Warung Terdekat.
+        </p>
+        <div className="flex font-bold justify-center items-center w-fit">
+          <FaLocationDot className='text-3xl mr-4' />
+          <p>Desa Gondanglegi, Kecamatan Klego, Kabupaten Boyolali, Jawa Tengah</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
